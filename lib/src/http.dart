54e48a2eb2994942472076e3bcb69f416620fd48
renato <renato.athaydes@curity.io>
2019-08-30 08:37:20 +0200

    Improved repository search.

--- lib/src/http.dart
+++ lib/src/http.dart
@@ -8,8 +8,8 @@ Future<http.Response> get(dynamic url,
         {Map<String, String> headers = const {'Accept': githubJsonV3}}) =>
     http.get(url, headers: headers);
 
-Future<http.Response> findRepoByTopic(String tag) =>
-    get('${githubUrl}/search/topics?q=$tag',
+Future<http.Response> findRepoByTopic(String topic) =>
+    get('${githubUrl}/search/repositories?q=topic:$topic',
         headers: const {'Accept': githubJsonMercyPreview});
 
 Future<http.Response> findUser(String user) => get('${githubUrl}/users/$user');
