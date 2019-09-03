b13dd71b2b4c839042f97ae736a7e299b45ae9fa
renato <renato.athaydes@curity.io>
2019-09-03 23:00:02 +0200

    Created Object to represent repositories response.

--- lib/src/repos.dart
+++ lib/src/repos.dart
@@ -18,6 +18,67 @@ const _repositorySummary = {
   'URL': 'html_url',
 };
 
+class RepoResponse {
+  final http.Response resp;
+  List _repos;
+  int _totalCount;
+
+  RepoResponse(this.resp);
+
+  void _readBodyIfNeeded() {
+    if (_repos != null) return;
+    if (isError) {
+      _repos = const [];
+      _totalCount = 0;
+    } else {
+      final json = jsonDecode(resp.body);
+      _repos = _reposFrom(json);
+      _totalCount = _totalCountFrom(json);
+    }
+  }
+
+  bool get isError => resp.statusCode != 200;
+
+  bool get isNotError => !isError;
+
+  List get repos {
+    _readBodyIfNeeded();
+    return _repos;
+  }
+
+  String get nextPage => linkToNextPage(resp.headers);
+
+  int get totalCount {
+    _readBodyIfNeeded();
+    return _totalCount;
+  }
+
+  List<String> get linksToSubscribers {
+    return repos
+        .map((r) => r["subscribers_url"]?.toString())
+        .where((s) => s != null)
+        .toList();
+  }
+
+  bool get isEmpty => totalCount == 0;
+
+  static List _reposFrom(json) {
+    if (json is List) {
+      return json;
+    } else {
+      return json["items"] as List;
+    }
+  }
+
+  static int _totalCountFrom(json) {
+    if (json is List) {
+      return json.length;
+    } else {
+      return int.tryParse(json["total_count"]?.toString());
+    }
+  }
+}
+
 class RepoSearch with MenuItem {
   bool verbose;
   MenuItem _prev;
@@ -52,48 +113,39 @@ class RepoSearch with MenuItem {
 class ShowRepos with MenuItem {
   final bool verbose;
   final MenuItem _prev;
-  dynamic _json;
-  String _nextPage;
+  RepoResponse _resp;
 
   ShowRepos(this.verbose, this._prev, http.Response response) {
     _updateResponse(response);
   }
 
-  bool get isEmptyResponse => _reposFrom(_json).isEmpty;
+  bool get isEmptyResponse => _resp.isEmpty;
 
   void _updateResponse(http.Response resp) {
-    _nextPage = linkToNextPage(resp.headers);
-    _json = jsonDecode(resp.body);
-  }
-
-  static List _reposFrom(json) {
-    if (json is List) {
-      return json;
-    } else {
-      return json["items"] as List;
-    }
+    _resp = RepoResponse(resp);
   }
 
   @override
   void ask() {
     print("Enter the name of a repository to show more information about it,\n"
             "\\top to go back to the main menu" +
-        (_nextPage == null ? '.' : ",\n\\next to see the next repositories."));
+        (_resp.nextPage == null
+            ? '.'
+            : ",\n\\next to see the next repositories."));
   }
 
   @override
   FutureOr<MenuItem> call(String answer) async {
     if (answer == '\\next') {
-      if (_nextPage == null) {
+      if (_resp.nextPage == null) {
         warn("There is no next page to go to.");
       } else {
-        _updateResponse(await get(_nextPage, verbose: verbose));
+        _updateResponse(await get(_resp.nextPage, verbose: verbose));
         showRepositories();
       }
     } else {
-      final items = _reposFrom(_json);
-      final re = items.firstWhere((repo) => repo['name'] == answer,
-          orElse: () => null);
+      final re = _resp.repos
+          .firstWhere((repo) => repo['name'] == answer, orElse: () => null);
       if (re == null) {
         warn("Cannot find this repository, please try again.");
       } else {
@@ -107,17 +159,11 @@ class ShowRepos with MenuItem {
   MenuItem prev() => _prev;
 
   void reportRepositoriesCount() {
-    int reposFound;
-    final json = _json;
-    if (json is List) {
-      reposFound = json.length;
-    } else {
-      reposFound = int.tryParse(json["total_count"]?.toString());
-    }
+    int reposFound = _resp.totalCount;
     print("Found ${reposFound ?? '?'} repositories.");
   }
 
-  void showRepositories() => _showRepositories(_reposFrom(_json));
+  void showRepositories() => _showRepositories(_resp.repos);
 
   void _showRepositories(List items) {
     for (final repo in items) {
